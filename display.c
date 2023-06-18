9a1b177ea2936f7d85e6a89412eaa0483d5b3282
nayeawon <yea1189@gmail.com>
2023-06-19 06:42:22 +0900

    Add features for displaying board using ncurses

--- display.c
+++ display.c
@@ -1,4 +1,5 @@
 #include <ncurses.h>
+#include "game.h"
 
 void display(int board[8][8])
 {
@@ -23,4 +24,31 @@ void display(int board[8][8])
         printw("+---");
     }
     printw("+\n");
+}
+
+int next_move(Coordinate * pos[64])
+{
+    // Enable attributes for blinking text
+    attron(A_BLINK);
+
+    for (int i = 0 ; i < 64 ; i++) {
+        if (pos[i] == NULL)
+            break ;
+        int x = 1 + 2 * pos[i]->dst_x ;
+        int y = 2 + 4 * pos[i]->dst_y ;
+        mvprintw(x, y, ".") ;
+    }
+
+    refresh() ;
+
+    napms(1000) ;
+
+    attroff(A_BLINK) ;
+
+    refresh() ;
+
+    int ch = getch() ;
+    clear() ;
+
+    return ch ;
 }
--- display.h
+++ display.h
@@ -1,1 +1,3 @@
 void display(int board[8][8]) ;
+
+int next_move(Coordinate * pos[64]) ;
