f05ae545e9f0222e27fd00ce51a9f7d787a59164
nayeawon <yea1189@gmail.com>
2023-06-19 21:09:30 +0900

    Add comments on functions

--- display.h
+++ display.h
@@ -1,5 +1,8 @@
+/* print out the board */
 void display(int board[8][8]) ;
 
+/* indicate the next move that user can select */
 int next_move(int board[8][8], Coordinate * pos[64], int color) ;
 
+/* gradually show the change of board */
 void display_new(Type type, int changes[8], int color, int (* old_board)[8]) ;
